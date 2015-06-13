165ab221f691227f29d8ba869aa03e6ac29ba3f7
Jamie Mansfield <malgm@lapis.blue>
2015-06-13 15:07:00 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='korobo_docs_theme',
+    version='0.0.1',
+    packages=['korobi_docs_theme'],
+    url='https://github.com/korobi/korobi_docs_theme',
+    license='MIT',
+    author='Jamie',
+    author_email='jamie@neptunepowered.org',
+    description='The Korobi documentation theme.'
+)
