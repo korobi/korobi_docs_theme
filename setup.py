8a435639ebee91bd1ec6084467c9cf48e4451145
Jamie Mansfield <malgm@lapis.blue>
2015-06-13 16:04:15 +0100

    Fix footer

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='korobo_docs_theme',
+    name='korobi_docs_theme',
     version='0.0.1',
     packages=['korobi_docs_theme'],
     url='https://github.com/korobi/korobi_docs_theme',
